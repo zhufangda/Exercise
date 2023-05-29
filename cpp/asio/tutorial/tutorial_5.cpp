9c9a1de29a239bab838a92f8ad732d64b215c8e8
zhufangda <zhufangda@didiglobal.com>
2023-05-29 20:28:05 +0800

    WIP

--- cpp/asio/tutorial/tutorial_5.cpp
+++ cpp/asio/tutorial/tutorial_5.cpp
@@ -1,61 +1,67 @@
-#include <boost/asio.hpp>
-#include <boost/bind/bind.hpp>
-#include <boost/thread/thread.hpp>
 #include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/bind/bind.hpp>
 
 #include "thread.h"
 
-class Printer {
+class printer
+{
 public:
-  explicit Printer(std::shared_ptr<boost::asio::io_context> &io)
-      : io_service_(io),
-        timer1_(*io, boost::asio::chrono::seconds(1)),
-        timer2_(*io, boost::asio::chrono::seconds(1)), count_(0) {
+  explicit printer(boost::asio::io_context& io)
+      : strand_(boost::asio::make_strand(io)),
+        timer1_(io, boost::asio::chrono::seconds(1)),
+        timer2_(io, boost::asio::chrono::seconds(1)),
+        count_(0)
+  {
 
-    timer1_.async_wait(boost::asio::bind_executor(
-        *io, boost::bind(&Printer::print1, this)));
+    timer1_.async_wait(boost::asio::bind_executor(strand_,
+                                                  boost::bind(&printer::print1, this)));
 
-    timer2_.async_wait(boost::asio::bind_executor(
-        *io, boost::bind(&Printer::print2, this)));
+    timer2_.async_wait(boost::asio::bind_executor(strand_,
+                                                  boost::bind(&printer::print2, this)));
   }
-  void print1() {
-    if (count_ < 10) {
-      std::cout << "Timer 1: " << count_ << " thread=" << GetThreadSimpleId() << std::endl;
+  void print1()
+  {
+    if (count_ < 10)
+    {
+      std::cout << "Timer 1: " << count_ << std::endl;
       ++count_;
 
       timer1_.expires_at(timer1_.expiry() + boost::asio::chrono::seconds(1));
 
-      timer1_.async_wait(boost::asio::bind_executor(
-          *io_service_, boost::bind(&Printer::print1, this)));
+      timer1_.async_wait(boost::asio::bind_executor(strand_,
+                                                    boost::bind(&printer::print1, this)));
     }
   }
 
-  void print2() {
-    if (count_ < 10) {
-      std::cout << "Timer 2: " << count_ << " thread=" << GetThreadSimpleId() << std::endl;
+  void print2()
+  {
+    if (count_ < 10)
+    {
+      std::cout << "Timer 2: " << count_ << std::endl;
       ++count_;
 
       timer2_.expires_at(timer2_.expiry() + boost::asio::chrono::seconds(1));
 
-      timer2_.async_wait(boost::asio::bind_executor(
-          *io_service_, boost::bind(&Printer::print2, this)));
+      timer2_.async_wait(boost::asio::bind_executor(strand_,
+                                                    boost::bind(&printer::print2, this)));
     }
   }
 
 private:
   boost::asio::strand<boost::asio::io_context::executor_type> strand_;
-  std::shared_ptr<boost::asio::io_service> io_service_;
   boost::asio::steady_timer timer1_;
   boost::asio::steady_timer timer2_;
   int count_;
 };
-
-int main() {
-  std::cout << "Main: thread=" << GetThreadSimpleId() << std::endl;
-  auto io = std::make_shared<boost::asio::io_context>();
-  Printer p(io);
-  boost::thread t(boost::bind(&boost::asio::io_context::run, io.get()));
-  io->run();
+int main()
+{
+  boost::asio::io_context io;
+  printer p(io);
+  boost::thread t(boost::bind(&boost::asio::io_context::run, &io));
+  io.run();
   t.join();
+
   return 0;
 }
--- cpp/asio/tutorial/tutorial_6.cpp
+++ cpp/asio/tutorial/tutorial_6.cpp
@@ -16,7 +16,7 @@ public:
       : strand_(ex.get_executor()) {
 
     boost::asio::spawn(strand_, [&ex](boost::asio::yield_context yield) {
-      boost::asio::steady_timer timer(ex, 20);
+      boost::asio::steady_timer timer(ex);
       boost::system::error_code error_code;
       while (!error_code) {
         timer.expires_from_now(std::chrono::milliseconds(20), error_code);
@@ -39,30 +39,30 @@ private:
 int main() {
   std::cout << "Main: thread=" << GetThreadSimpleId() << std::endl;
 
-  auto io_context_ = boost::asio::io_context;
+  auto io_context_ = boost::asio::io_context();
 
   boost::asio::signal_set signal_set(io_context_);
   boost::system::error_code error_code;
   signal_set.add(SIGINT, error_code);
 
   signal_set.async_wait(
-      [io_context_](const boost::system::error_code &ec, int signal_no) {
+      [&io_context_](const boost::system::error_code &ec, int signal_no) {
         DCHECK_EQ(SIGINT, signal_no);
-        io_context_->stop();
+        io_context_.stop();
       });
 
   Printer p(io_context_);
   
   std::vector<std::thread> thread_pool(10);
   for (auto& thread : thread_pool) {
-    thread = std::thread([io_context_]() {
-      io_context_->run();
+    thread = std::thread([&io_context_]() {
+      io_context_.run();
     });
   }
   
   boost::thread t(
-      boost::bind(&boost::asio::io_context::run, io_context_.get()));
-  io_context_->run();
+      boost::bind(&boost::asio::io_context::run, &io_context_));
+  io_context_.run();
   t.join();
   return 0;
 }
