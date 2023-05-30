52e84374d8073147631b7371c90a688ed800763f
zhufangda <zhufangda@didiglobal.com>
2023-05-30 14:00:22 +0800

    WIP

--- cpp/asio/tutorial/datetime_1.cpp
+++ cpp/asio/tutorial/datetime_1.cpp
@@ -3,35 +3,42 @@
 #include <iostream>
 
 using boost::asio::ip::tcp;
-
+// 创建client端以接收消息。
 int main(int argc, char *argv[]) {
   try {
+    // 检查用户是否提供服务端IP
     if (argc != 2) {
       std::cerr << "Usage: client <host>" << std::endl;
       return 1;
     }
 
     boost::asio::io_context io_context;
-
+    // 解析server name与host name
     tcp::resolver resolver(io_context);
     tcp::resolver::results_type endpoints =
         resolver.resolve(argv[1], "daytime");
+    std::cout << "[" << endpoints->service_name()
+              << "]:" << endpoints->host_name() << std::endl;
 
+    // 创建链接
     tcp::socket socket(io_context);
     boost::asio::connect(socket, endpoints);
 
-    for (;;) {
-      boost::array<char, 128> buf;
-      boost::system::error_code error;
+    for (int i = 0; i < 10; i++) {
+      for (;;) {
+        boost::array<char, 128> buf;
+        boost::system::error_code error;
 
-      size_t len = socket.read_some(boost::asio::buffer(buf), error);
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
 
-      if (error == boost::asio::error::eof)
-        break; // Connection closed cleanly by peer.
-      else if (error)
-        throw boost::system::system_error(error); // Some other error.
+        if (error == boost::asio::error::eof)
+          break; // Connection closed cleanly by peer.
+        else if (error)
+          throw boost::system::system_error(error); // Some other error.
 
-      std::cout.write(buf.data(), len);
+        std::cout.write(buf.data(), len);
+      }
+      std::cout << std::endl;
     }
   } catch (std::exception &e) {
     std::cerr << e.what() << std::endl;
---
+++ cpp/asio/tutorial/datetime_2.cpp
@@ -0,0 +1,36 @@
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <ctime>
+#include <iostream>
+#include <string>
+
+using boost::asio::ip::tcp;
+
+std::string make_daytime_string() {
+  using namespace std; // For time_t, time and ctime;
+  time_t now = time(0);
+  return ctime(&now);
+}
+
+int main() {
+  try {
+    boost::asio::io_context io_context;
+
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 13));
+
+    for (;;) {
+      tcp::socket socket(io_context);
+      acceptor.accept(socket);
+
+      std::string message = make_daytime_string();
+
+      boost::system::error_code ignored_error;
+      boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+      std::cout << "Send -- " << message << std::endl;
+    }
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+
+  return 0;
+}
